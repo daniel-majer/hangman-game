c458ce831fdd1a76bc33387e584e38293bcbfea1
dnomjr <dano.majer@protonmail.com>
2024-11-02 18:00:45 -0400

    switch solution → menu - instructions

---
+++ app.js
@@ -0,0 +1,43 @@
+class Return {
+  constructor() {
+
+    /* SECTIONS */
+    this.introSection = document.querySelector('.main-menu')
+    this.introContainer = document.querySelector('.intro-container')
+    this.instructionsSection = document.querySelector('.instructions')
+
+    /* BUTTONS */
+    this.howToPlayBtn = document.querySelector('.instructions-btn')
+    this.backToMenuBtn = document.querySelector('.back-btn')
+
+    /* EVENTS */
+    this.howToPlayBtn.addEventListener(
+      'click',
+      this.onForward.bind(this, 'instructions')
+    )
+    this.backToMenuBtn.addEventListener('click', this.onBack.bind(this))
+    document.addEventListener('DOMContentLoaded', this.load.bind(this))
+  }
+
+  onBack() {
+    this.instructionsSection.classList.toggle('show-instructions')
+    this.introSection.classList.toggle('show-menu')
+    this.load()
+  }
+
+  onForward(page) {
+    if (page === 'instructions') {
+      this.instructionsSection.classList.toggle('show-instructions')
+      this.introSection.classList.toggle('show-menu')
+      this.instructionsSection.style.height = ''
+      console.log(page)
+    }
+  }
+
+  load() {
+    const height = this.introSection.getBoundingClientRect().height - 1
+    this.instructionsSection.style.height = height + 'px'
+  }
+}
+
+const start = new Return()
