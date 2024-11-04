d086b1f57d472ad1457f1d232fd223e96b37a221
dnomjr <dano.majer@protonmail.com>
2024-11-04 15:42:21 -0500

    solved navigation problem

--- app.js
+++ app.js
@@ -1,44 +1,82 @@
-class Return {
+function getElement(el) {
+  const element = document.querySelector(el)
+  if (element) return element
+  throw new Error(`Please check "${el}" selector, no such element exists`)
+}
+
+class Navigation {
   constructor() {
     /* SECTIONS */
-    this.introSection = document.querySelector('.main-menu')
-    this.introContainer = document.querySelector('.intro-container')
-    this.instructionsSection = document.querySelector('.instructions')
+    this.menuSection = getElement('.main-menu')
+    this.instructionsSection = getElement('.instructions')
+    this.categoriesSection = getElement('.categories')
+    this.allSections = document.querySelectorAll('.page')
 
     /* BUTTONS */
-    this.howToPlayBtn = document.querySelector('.instructions-btn')
-    this.backToMenuBtn = document.querySelector('.back-btn')
+    this.playBtn = getElement('.categories-btn')
+    this.instructionsBtn = getElement('.instructions-btn')
+    this.backBtn = document.querySelectorAll('.menu-btn')
+
+    /* OTHERS */
+    this.current = 'current'
 
     /* EVENTS */
-    this.howToPlayBtn.addEventListener(
-      'click',
-      this.onForward.bind(this, 'instructions')
-    )
-    this.backToMenuBtn.addEventListener('click', this.onBack.bind(this))
-    document.addEventListener('DOMContentLoaded', this.load.bind(this))
+
+    /* FORWARD */
+    this.instructionsBtn.addEventListener('click', this.onForward.bind(this))
+    this.playBtn.addEventListener('click', this.onForward.bind(this))
+    /* BACK */
+    this.backBtn.forEach(btn => {
+      btn.addEventListener('click', this.onBack.bind(this))
+    })
+    /* this.backBtn.addEventListener('click', this.onBack.bind(this)) */
+    /* LOAD */
+    document.addEventListener('DOMContentLoaded', this.onLoad.bind(this))
   }
 
-  onBack() {
-    this.instructionsSection.classList.toggle('show-instructions')
-    this.introSection.classList.toggle('show-menu')
-    this.load()
-  }
-
-  onForward(page) {
-    if (page === 'instructions') {
-      this.instructionsSection.classList.toggle('show-instructions')
-      this.introSection.classList.toggle('show-menu')
-      this.instructionsSection.style.height = ''
-      console.log(page)
+  nextStep(side, func) {
+    if (this.btn) {
+      this[`${this.btn}Section`].dataset.set = this.current
+      this[`${this.btn}Section`].classList[func](`swipe-${side}`)
     }
   }
 
-  load() {
-    const height = this.introSection.getBoundingClientRect().height - 1
-    this.instructionsSection.style.height = height + 'px'
-    console.log(this.a.a)
+  findCurrentSection(e) {
+    this.btn = e.target?.classList[0]?.split('-')[0]
+
+    let sections = document.querySelectorAll('section')
+    sections = [...sections]
+
+    this.currentSection = sections.find(
+      section => section.dataset.set === 'current'
+    )
+    this.currentSection.dataset.set = ''
   }
-  
+
+  onBack(e) {
+    this.findCurrentSection(e)
+    this.currentSection.classList.remove('swipe-right')
+    this.nextStep('left', 'remove')
+  }
+
+  onForward(e) {
+    this.findCurrentSection(e)
+    this.currentSection.classList.add('swipe-left')
+    this.nextStep('right', 'add')
+  }
+
+  onLoad() {
+    const height = this.menuSection.getBoundingClientRect().height - 1
+    this.allSections.forEach(function (section) {
+      section.style.height = height + 'px'
+    })
+  }
 }
 
-const start = new Return()
+class App {
+  static init() {
+    const start = new Navigation()
+  }
+}
+
+App.init()
